2536f669a6032887b0bbcddf7b8de00402617132
Angel Gallego <angelgb1990@gmail.com>
2024-11-10 19:53:48 +0000

    terminado fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,47 +4,106 @@ import { faTrash, } from "@fortawesome/free-solid-svg-icons";
 //create your first component
 const Home = () => {
 
+	const [Usuario, setUsuario] = useState("AngelGB")
 	const [imputValue, setImputValue] = useState("")
 	const [todoList, setTodoList] = useState([])
+	const [cargaTodoList, setCargaTodoList] = useState([])
 
-	const post = {
+	const postTodo = {
 		label: imputValue,
 		is_done: false
-	  }
-	useEffect(() => {
-		fetch("https://playground.4geeks.com/todo/users/AngelGB")
-			.then(response => response.json())
-			.then(response => setTodoList(response.todos))
-			.catch(error => console.log(error()));
+	}
 
-	}, [imputValue, eliminarTodo])
-	console.log(todoList);
-	
+
+	fetch(`https://playground.4geeks.com/todo/users/${Usuario}`)
+		.then(response => {
+			if (response.status === 404) {
+				return fetch(`https://playground.4geeks.com/todo/users/${Usuario}`, {
+					method: "POST"
+				});
+			}
+		})
+		.then(response => {
+			if (response) {
+				return response.json();
+			}
+		})
+		.then(data => {
+			if (data) {
+				console.log("Usuario creado:", data);
+			}
+		})
+		.catch(error => console.log(error));
+
+
+	const cargarTareas = () => {
+		fetch(`https://playground.4geeks.com/todo/users/${Usuario}`)
+			.then(response => response.json())
+			.then(response => setCargaTodoList(response.todos))
+			.catch(error => console.log(error));
+	};
+
+	useEffect(() => {
+		cargarTareas();
+	}, []);
+
 	const CrearTodo = () => {
 		fetch("https://playground.4geeks.com/todo/todos/AngelGB", {
 			method: "POST",
-			headers: {"Content-Type":"application/json"},
-			body: JSON.stringify(post)
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(postTodo)
 		})
-		.then(response => response.json())
-		.then(response => console.log(response))
-		.catch(error => console.log(error()))
-	
+			.then(response => response.json())
+			.then(() => cargarTareas())
+			.catch(error => console.log(error()))
+
 	}
 
 	const eliminarTodo = (id) => {
 		fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
 			method: "DELETE",
 		})
-		.then(response => response.json())
-		.then(response => console.log(response))
-		.catch(error => console.log(error));
+			.then(response => {
+				if (response.ok) {
+					cargarTareas();
+				} else {
+					console.log("Error al eliminar la tarea");
+				}
+			})
+			.catch(error => console.log(error));
 	};
-console.log("obj de label",);
+
+	const ReiniciarTodo = () => {
+		fetch(`https://playground.4geeks.com/todo/users/${Usuario}`, {
+			method: "DELETE"
+		})
+			.then(response => {
+				if (response.ok) {
+					cargarTareas();
+				} else {
+					console.log("Error al eliminar la tarea");
+				}
+			})
+			.catch(error => console.log(error))
+	}
+
+	const limpiarTareas = () => {
+		fetch(`https://playground.4geeks.com/todo/users/${Usuario}`, {
+			method: "DELETE",
+		})
+			.then(response => {
+				if (response.ok) {
+					setCargaTodoList([]);
+				} else {
+					console.log("Error al eliminar todas las tareas");
+				}
+			})
+			.catch(error => console.log(error));
+	};
 
 	return (
 		<div>
-			<div className="cabecero"><h1 className="text-center p-5">Todo List</h1></div>
+			<div className="cabecero"><h1 className="text-center p-5">Todo List con Fetch</h1></div>
 			<div className=" hoja hoja3 p-4">
 				<div className="container input-group flex-nowrap p-3 ps-5">
 					<span className="input-group-text" id="addon-wrapping">Introduce tareas</span>
@@ -66,15 +125,15 @@ console.log("obj de label",);
 					<div className="ps-3 container">
 						<ul><h3 className="p-3 text-center">Tareas pendientes...</h3>
 							{
-								todoList.length === 0 ? (
-									<p className="text-center">¡No quedan tareas pendientes! <img src="https://media.tenor.com/12vvBX1HaPUAAAAi/dance.gif" alt="" /> </p>
+								cargaTodoList.length === 0 ? (
+									<p className="text-center">¡No quedan tareas pendientes!</p>
 								) :
-									todoList.map((item, index) => {
+									cargaTodoList.map((item, index) => {
 										return (
 											<h5 key={index} className="tareapendiente">
 												<input className="form-check-input" type="checkbox" />
 												- {item.label}
-												<FontAwesomeIcon onClick={() => {eliminarTodo(item.id)}}
+												<FontAwesomeIcon onClick={() => { eliminarTodo(item.id) }}
 													className="eliminar float-end"
 													icon={faTrash} />
 											</h5>
@@ -82,13 +141,13 @@ console.log("obj de label",);
 									})}
 						</ul>
 					</div>
-					<div className="container text-end">
-						<p>{todoList.length} tareas restantes </p>
+					<div className="row p-5 d-flex justify-content-center">
+						<p className="text-end">{cargaTodoList.length} tareas restantes </p>
+						<button onClick={() => { limpiarTareas() }} className="col-3  btn btn-primary">Reiniciar lista</button>
 					</div>
 				</div>
-
 			</div>
-			{/* <div className="hoja hoja2 p-4">
+			<div className="hoja hoja2 p-4">
 				<div className="container input-group flex-nowrap p-3 ps-5">
 					<span className="input-group-text" id="addon-wrapping">Introduce tareas</span>
 					<input type="text" className="form-control" placeholder="Ejemplo programar 35 horas al dia" />
@@ -98,7 +157,7 @@ console.log("obj de label",);
 						<ul><h3 className="p-3 text-center">Tareas pendientes...</h3>
 							{
 								todoList.length === 0 ? (
-									<p className="text-center">¡No quedan tareas pendientes! <img src="https://media.tenor.com/12vvBX1HaPUAAAAi/dance.gif" alt="" /> </p>
+									<p className="text-center">¡No quedan tareas pendientes!</p>
 								) :
 									todoList.map((item, index) => {
 										return (
@@ -108,8 +167,9 @@ console.log("obj de label",);
 									})}h5
 						</ul>
 					</div>
-					<div className="container text-end">
-						<p>tareas restantes </p>
+					<div className="row p-5 d-flex justify-content-center">
+						<p className="text-end">{todoList.length} tareas restantes </p>
+						<button onClick={() => { limpiarTareas() }} className="col-3  btn btn-primary">Reiniciar lista</button>
 					</div>
 				</div>
 			</div>
@@ -123,7 +183,7 @@ console.log("obj de label",);
 						<ul><h3 className="p-3 text-center">Tareas pendientes...</h3>
 							{
 								todoList.length === 0 ? (
-									<p className="text-center">¡No quedan tareas pendientes! <img src="https://media.tenor.com/12vvBX1HaPUAAAAi/dance.gif" alt="" /> </p>
+									<p className="text-center">¡No quedan tareas pendientes! </p>
 								) :
 									todoList.map((item, index) => {
 										return (
@@ -133,11 +193,14 @@ console.log("obj de label",);
 									})}
 						</ul>
 					</div>
-					<div className="container text-end">
-						<p>tareas restantes </p>
+					<div className="row p-5 d-flex justify-content-center">
+						<p className="text-end">{todoList.length} tareas restantes </p>
+						<button onClick={() => { limpiarTareas() }} className="col-3  btn btn-primary">Reiniciar lista</button>
 					</div>
 				</div>
-			</div> */}
+			</div>
+
+
 		</div>
 	);
 };
